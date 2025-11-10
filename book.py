dbd018f351c6bd29c60e3822d0bacbd690c6dfab
Ariel <arielhalifa@gmail.com>
2025-11-10 11:32:03 +0200

    creating book class

---
+++ book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author, isbn, is_available):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.is_available = is_available
+
+    def __str__(self):
+        return f"""title: {self.title}
+author: {self.author}
+isbn: {self.isbn}
+is available?: {self.is_available}"""
